fe4df0adaa559076a811ca6a4992b6fc0b925ccb
Pete Smith <pete.smith@digital.hmrc.gov.uk>
2016-05-17 10:34:05 +0100

    [PS] Upgrade to microservice boostrap 6.4.0

--- project/MicroServiceBuild.scala
+++ project/MicroServiceBuild.scala
@@ -23,7 +23,7 @@ private object AppDependencies {
   val compile = Seq(
 
     ws,
-    "uk.gov.hmrc" %% "frontend-bootstrap" % "6.1.0",
+    "uk.gov.hmrc" %% "frontend-bootstrap" % "6.4.0",
     "uk.gov.hmrc" %% "play-config" % "2.0.1",
     "uk.gov.hmrc" %% "play-json-logger" % "2.1.1",
     "uk.gov.hmrc" %% "play-health" % "1.1.0",
