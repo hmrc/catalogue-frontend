2f4d1104a7c7d4c5213deec59f1623e8d9b9474a
ashu.singh <asingh@equalexperts.com>
2016-07-07 09:07:14 +0100

    PLATOPS-542 changed the text label

--- test/uk/gov/hmrc/cataloguefrontend/TeamServicesSpec.scala
+++ test/uk/gov/hmrc/cataloguefrontend/TeamServicesSpec.scala
@@ -61,7 +61,7 @@ class TeamServicesSpec extends UnitSpec with BeforeAndAfter with OneServerPerTes
       response.status shouldBe 200
       response.body should include(s"Last updated at: Tue, 14 Oct 1066 10:03:23 GMT")
 
-      response.body.toString should include("""<a href=http://usermanagement/link/team-teama target="_blank">team members</a>""")
+      response.body.toString should include("""<a href=http://usermanagement/link/team-teama target="_blank">Team Members</a>""")
 
     }
 
