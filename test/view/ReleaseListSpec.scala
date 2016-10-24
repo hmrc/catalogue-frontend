f8abf4166db64f76526a3578a4371f1dd6cbc9cf
Pete Smith <pete.smith@digital.hmrc.gov.uk>
2016-10-24 11:29:47 +0100

    [PS] Switched the column headers for team and service around

--- test/view/ReleaseListSpec.scala
+++ test/view/ReleaseListSpec.scala
@@ -48,8 +48,8 @@ class ReleaseListSpec extends WordSpec with Matchers {
 
       val document = asDocument(views.html.release_list.render(Seq(TeamRelease("serv1", Seq("teamA", "teamB"), productionDate = now, creationDate = Some(now.plusDays(2)), interval = Some(2), leadTime = Some(10), version = "1.0")), ReleasesFilter.form))
 
-      document.select("#row0_name").text() shouldBe "serv1"
       document.select("#row0_team").text() shouldBe "teamA teamB"
+      document.select("#row0_name").text() shouldBe "serv1"
       document.select("#row0_version").text() shouldBe "1.0"
       document.select("#row0_production").text() shouldBe now.asString
       document.select("#row0_creation").text() shouldBe now.plusDays(2).asString
