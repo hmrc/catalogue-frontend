a4f7ccbce49fc6949009f796425d5c7c6e64ec5d
Armin Keyvanloo <armin.keyvanloo@gmail.com>
2016-10-25 16:27:14 +0100

    [PLATOPS-743] updated the sbt resolvers for play 2.5

--- project/MicroServiceBuild.scala
+++ project/MicroServiceBuild.scala
@@ -48,7 +48,6 @@ private object AppDependencies {
       override lazy val test = Seq(
         "uk.gov.hmrc" %% "hmrctest" % hmrcTestVersion % scope,
         "org.scalatest" %% "scalatest" % "2.2.6" % scope,
-//        "org.scalatestplus" %% "play" % "1.2.0" % scope,
         "org.scalatestplus.play" %% "scalatestplus-play" % "1.5.0" % scope,
         "org.pegdown" % "pegdown" % "1.4.2" % scope,        
         "com.typesafe.play" %% "play-test" % PlayVersion.current % scope,
