60742b6a9d86606351f1a1a535da096198945662
ashu.singh <asingh@equalexperts.com>
2016-11-03 16:07:28 +0000

    added ump update link and removed feature toggle

---
+++ app/uk/gov/hmrc/cataloguefrontend/UserManagementPortalLink.scala
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2016 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.cataloguefrontend
+
+import uk.gov.hmrc.play.config.ServicesConfig
+
+trait UserManagementPortalLink extends  ServicesConfig{
+  val serviceName = "user-management"
+
+  def umpFrontPageUrl(teamName: String): String = {
+
+    getConfString(s"$serviceName.frontPageUrl", "#") + s"/$teamName"
+
+  }
+
+  def userManagementBaseUrl: String =
+    getConfString(s"$serviceName.url", throw new RuntimeException(s"Could not find config $serviceName.url"))
+
+
+}
