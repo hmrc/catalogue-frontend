3e81f8e95edb3730afb5a95fa8b543a8e3c86783
thomas.vogel <thomas.vogel@digital.hmrc.gov.uk>
2016-11-11 10:01:07 +0000

    [PLATOPS-774] Changed the no production deployment error message on the service page to lower case

--- app/uk/gov/hmrc/cataloguefrontend/ViewMessages.scala
+++ app/uk/gov/hmrc/cataloguefrontend/ViewMessages.scala
@@ -52,7 +52,7 @@ object ViewMessages {
   }
 
   def noProductionDeploymentFor(numberOfDays: Long) = {
-    Html(s"""<h2 class="chart-message text-center">NO PRODUCTION DEPLOYMENTS FOR $numberOfDays DAYS</h2>""" + s"<p>${ViewMessages.noIndicatorsData}</p>")
+    Html(s"""<h2 class="chart-message text-center">No production deployments for $numberOfDays days</h2>""" + s"<p>${ViewMessages.noIndicatorsData}</p>")
   }
 
   def errorMessage = {
