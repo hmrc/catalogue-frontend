67b29cd811225a92dde72b002a9a80a64245e504
Armin Keyvanloo <armin.keyvanloo@gmail.com>
2017-05-16 17:01:09 +0100

    PLATOPS-989 adding tests for the error cases of in the Digital Service details page

--- test/uk/gov/hmrc/cataloguefrontend/DigitalServicePageSpec.scala
+++ test/uk/gov/hmrc/cataloguefrontend/DigitalServicePageSpec.scala
@@ -21,20 +21,29 @@ import java.time.ZoneOffset
 import com.github.tomakehurst.wiremock.http.RequestMethod._
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
+import org.mockito
+import org.mockito.Matchers._
+import org.mockito.Mockito
+import org.mockito.Mockito.when
 import org.scalatest._
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.mock.MockitoSugar
 import org.scalatestplus.play.OneServerPerSuite
 import play.api.inject.guice.GuiceApplicationBuilder
 import play.api.libs.json.Json
 import play.api.libs.ws.WS
+import play.api.mvc.{Action, AnyContent, Result}
+import play.api.test.FakeRequest
 import uk.gov.hmrc.cataloguefrontend.DateHelper._
 import uk.gov.hmrc.cataloguefrontend.JsonData._
 import uk.gov.hmrc.cataloguefrontend.UserManagementConnector.TeamMember
 import uk.gov.hmrc.play.test.UnitSpec
 
 import scala.collection.JavaConversions._
+import scala.concurrent.Future
 import scala.io.Source
 
-class DigitalServicePageSpec extends UnitSpec with BeforeAndAfter with OneServerPerSuite with WireMockEndpoints {
+class DigitalServicePageSpec extends UnitSpec with BeforeAndAfter with OneServerPerSuite with WireMockEndpoints with MockitoSugar with ScalaFutures {
 
   def asDocument(html: String): Document = Jsoup.parse(html)
 
@@ -143,6 +152,122 @@ class DigitalServicePageSpec extends UnitSpec with BeforeAndAfter with OneServer
       response.body should include(ViewMessages.noRepoOfTypeForDigitalService("prototype"))
       response.body should include(ViewMessages.noRepoOfTypeForDigitalService("other"))
     }
+
+    "show team members for teams correctly" in {
+      val team1 = "Team1"
+      val team2 = "Team2"
+
+      val teamNames = Seq(team1, team2)
+      val digitalServiceName = "digital-service-123"
+
+
+      val json =
+        s"""
+          |{
+          |  "name": "Catalogue",
+          |  "lastUpdatedAt": 1494864193000,
+          |  "repositories": [
+          |    {
+          |      "name": "catalogue-frontend",
+          |      "createdAt": 1456326530000,
+          |      "lastUpdatedAt": 1494864193000,
+          |      "repoType": "Service",
+          |      "teamNames": [ "$team1", "$team2"]
+          |    }
+          |  ]
+          |}
+        """.stripMargin
+      serviceEndpoint(GET, s"/api/digital-services/$digitalServiceName", willRespondWith = (200, Some(
+        json
+      )), extraHeaders = Map("X-Cache-Timestamp" -> "Fri, 14 Oct 1983 10:03:23 GMT"))
+
+
+      mockHttpApiCall(s"/v1/organisations/mdtp/teams/${team1}/members", "/user-management-response-team1.json")
+      mockHttpApiCall(s"/v1/organisations/mdtp/teams/${team2}/members", "/user-management-response-team2.json")
+
+
+      val response = await(WS.url(s"http://localhost:$port/digital-service/${digitalServiceName}").get)
+
+      response.status shouldBe 200
+      val document = asDocument(response.body)
+
+
+      verifyTeamMemberElementsText(document)
+
+      verifyTeamMemberHrefLinks(document)
+
+      verifyTeamOwnerIndicatorLabel(document)
+    }
+
+    "show the right error message when unable to connect to teams-and-repositories" in {
+
+      val digitalServiceName = "digital-service-123"
+
+      val teamsAndRepositoriesConnectorMock = mock[TeamsAndRepositoriesConnector]
+
+      val catalogueController = new CatalogueController {
+        override def userManagementConnector: UserManagementConnector = ???
+        override def teamsAndRepositoriesConnector: TeamsAndRepositoriesConnector = teamsAndRepositoriesConnectorMock
+        override def indicatorsConnector: IndicatorsConnector = ???
+        override def deploymentsService: DeploymentsService = ???
+      }
+
+      val exception = new RuntimeException("Boooom!")
+
+      //import uk.gov.hmrc.cataloguefrontend.TeamsAndRepositoriesConnector.ConnectionError
+
+      when(teamsAndRepositoriesConnectorMock.digitalServiceInfo(any())(any())).thenReturn(
+        Future.successful(
+          Left(TeamsAndRepositoriesConnector.ConnectionError(exception))
+        )
+      )
+
+      val response: Result = catalogueController.digitalService(digitalServiceName)(FakeRequest()).futureValue
+
+      response.header.status shouldBe 200
+
+      import play.api.test.Helpers._
+      val document: Document = asDocument(contentAsString(response))
+
+      document.select("#teams-and-repositories-error").text() should === (s"""A connection error to the teams-and-repositories microservice occurred while retrieving digital service ($digitalServiceName)""")
+    }
+
+    "show the right error message when unable to connect to ump" in {
+
+      val digitalServiceName = "digital-service-123"
+
+      val teamsAndRepositoriesConnectorMock = mock[TeamsAndRepositoriesConnector]
+      val umpConnectorMock = mock[UserManagementConnector]
+
+      val catalogueController = new CatalogueController {
+        override def userManagementConnector: UserManagementConnector = umpConnectorMock
+        override def teamsAndRepositoriesConnector: TeamsAndRepositoriesConnector = teamsAndRepositoriesConnectorMock
+        override def indicatorsConnector: IndicatorsConnector = ???
+        override def deploymentsService: DeploymentsService = ???
+      }
+
+      val teamName = "Team1"
+
+      val exception = new RuntimeException("Boooom!")
+      when(teamsAndRepositoriesConnectorMock.digitalServiceInfo(any())(any())).thenReturn(Future.successful(Right(Timestamped(DigitalService(digitalServiceName, 1, Nil), None))))
+      when(umpConnectorMock.getTeamMembersForTeams(any())(any())).thenReturn(
+      Future.successful(
+      Map(teamName -> Left(UserManagementConnector.ConnectionError(exception)))
+      )
+      )
+
+
+      val response: Result = catalogueController.digitalService(digitalServiceName)(FakeRequest()).futureValue
+      response.header.status shouldBe 200
+
+      import play.api.test.Helpers._
+
+      val document: Document = asDocument(contentAsString(response))
+
+      println(contentAsString(response))
+      document.select(s"#ump-error-$teamName").text() should === ("Sorry, the User Management Portal is not available")
+    }
+
   }
 
   def mockHttpApiCall(url: String, jsonResponseFile: String, httpCodeToBeReturned: Int = 200): String = {
@@ -161,4 +286,32 @@ class DigitalServicePageSpec extends UnitSpec with BeforeAndAfter with OneServer
   def readFile(jsonFilePath: String): String = {
     Source.fromURL(getClass.getResource(jsonFilePath)).getLines().mkString("\n")
   }
+
+  def verifyTeamOwnerIndicatorLabel(document: Document): Unit = {
+    val serviceOwnersLiLabels = document.select("#team_members li .label-success")
+    serviceOwnersLiLabels.size() shouldBe 2
+    serviceOwnersLiLabels.iterator().toSeq.map(_.text()) shouldBe Seq("Service Owner", "Service Owner")
+  }
+
+  def verifyTeamMemberHrefLinks(document: Document): Boolean = {
+    val hrefs = document.select("#team_members [href]").iterator().toList
+
+    hrefs.size shouldBe 4
+    hrefs(0).attributes().get("href") == "http://example.com/profile/joe.black"
+    hrefs(1).attributes().get("href") == "http://example.com/profile/james.roger"
+    hrefs(2).attributes().get("href") == "http://example.com/profile/casey.binge"
+    hrefs(3).attributes().get("href") == "http://example.com/profile/marc.pallazo"
+  }
+
+  def verifyTeamMemberElementsText(document: Document): Unit = {
+    val teamMembersLiElements = document.select("#team_members li").iterator().toList
+
+    teamMembersLiElements.length shouldBe 4
+
+    teamMembersLiElements(0).text() should include("Joe Black Service Owner")
+    teamMembersLiElements(1).text() should include("James Roger")
+    teamMembersLiElements(2).text() should include("Casey Binge Service Owner")
+    teamMembersLiElements(3).text() should include("Marc Palazzo")
+  }
+
 }
