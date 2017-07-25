88584c4cdb5ad97e758d08322bd5212c4a35a241
Armin Keyvanloo <armin.keyvanloo@gmail.com>
2017-07-25 09:36:15 +0100

    [PLATOPS-1026] moving connectors to an appropriate package

---
+++ app/uk/gov/hmrc/cataloguefrontend/connector/TeamsAndRepositoriesConnector.scala
@@ -0,0 +1,197 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.cataloguefrontend
+
+import java.net.URLEncoder
+import java.time.LocalDateTime
+
+import play.api.Logger
+import play.api.libs.json._
+import uk.gov.hmrc.cataloguefrontend.DigitalService.DigitalServiceRepository
+import uk.gov.hmrc.cataloguefrontend.TeamsAndRepositoriesConnector.ConnectionError
+import uk.gov.hmrc.cataloguefrontend.TeamsAndRepositoriesConnector.HTTPError
+import uk.gov.hmrc.cataloguefrontend.TeamsAndRepositoriesConnector.TeamsAndRepositoriesError
+
+import uk.gov.hmrc.cataloguefrontend.config.WSHttp
+import uk.gov.hmrc.play.config.ServicesConfig
+import uk.gov.hmrc.play.http._
+
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+object RepoType extends Enumeration {
+
+  type RepoType = Value
+
+  val Service, Library, Prototype, Other = Value
+
+  implicit val repoType = new Reads[RepoType] {
+    override def reads(json: JsValue): JsResult[RepoType] = json match {
+      case JsString(s) => JsSuccess(RepoType.withName(s))
+    }
+  }
+
+}
+
+case class Link(name: String, displayName: String, url: String)
+
+case class Environment(name: String, services: Seq[Link])
+
+case class RepositoryDetails(name: String,
+                             description: String,
+                             createdAt: LocalDateTime,
+                             lastActive: LocalDateTime,
+                             teamNames: Seq[String],
+                             githubUrls: Seq[Link],
+                             ci: Seq[Link],
+                             environments: Option[Seq[Environment]],
+                             repoType: RepoType.RepoType)
+
+case class RepositoryDisplayDetails(name:String, createdAt: LocalDateTime, lastUpdatedAt: LocalDateTime, repoType : RepoType.RepoType)
+object RepositoryDisplayDetails {
+  implicit val repoDetailsFormat = Json.format[RepositoryDisplayDetails]
+}
+
+case class Team(name:String, firstActiveDate: Option[LocalDateTime], lastActiveDate: Option[LocalDateTime], firstServiceCreationDate : Option[LocalDateTime], repos: Option[Map[String, Seq[String]]])
+object Team {
+  implicit val format = Json.format[Team]
+}
+
+case class DigitalService(name: String, lastUpdatedAt: Long, repositories: Seq[DigitalServiceRepository])
+
+object DigitalService {
+  case class DigitalServiceRepository(name:String, createdAt: LocalDateTime, lastUpdatedAt: LocalDateTime, repoType : RepoType.RepoType, teamNames: Seq[String])
+
+  implicit val repoDetailsFormat = Json.format[DigitalServiceRepository]
+
+
+  implicit val digitalServiceFormat = Json.format[DigitalService]
+}
+
+trait TeamsAndRepositoriesConnector extends ServicesConfig {
+  type ServiceName = String
+  type TeamName = String
+
+  val http: HttpGet with HttpPost
+
+  def teamsAndServicesBaseUrl: String
+
+  implicit val linkFormats = Json.format[Link]
+  implicit val environmentsFormats = Json.format[Environment]
+  implicit val serviceFormats = Json.format[RepositoryDetails]
+
+  val CacheTimestampHeaderName = "X-Cache-Timestamp"
+  implicit val httpReads: HttpReads[HttpResponse] = new HttpReads[HttpResponse] {
+    override def read(method: String, url: String, response: HttpResponse) = response
+  }
+
+  def allTeams(implicit hc: HeaderCarrier): Future[Timestamped[Seq[Team]]] = {
+    http.GET[HttpResponse](teamsAndServicesBaseUrl + s"/api/teams").map {
+      timestamp[Seq[Team]]
+    }
+  }
+
+  def allDigitalServices(implicit hc: HeaderCarrier): Future[Timestamped[Seq[String]]] = {
+    http.GET[HttpResponse](teamsAndServicesBaseUrl + s"/api/digital-services").map {
+      timestamp[Seq[String]]
+    }
+  }
+
+  def teamInfo(teamName: String)(implicit hc: HeaderCarrier): Future[Option[Timestamped[Team]]] = {
+    val url = teamsAndServicesBaseUrl + s"/api/teams_with_details/${URLEncoder.encode(teamName, "UTF-8")}"
+
+    http.GET[HttpResponse](url)
+      .map { response =>
+        response.status match {
+          case 404 => None
+          case 200 => Some(timestamp[Team](response))
+        }
+      }.recover {
+      case ex =>
+        Logger.error(s"An error occurred getting teamInfo when connecting to $url: ${ex.getMessage}", ex)
+        None
+    }
+  }
+
+  def digitalServiceInfo(digitalServiceName: String)(implicit hc: HeaderCarrier): Future[Either[TeamsAndRepositoriesError, Timestamped[DigitalService]]] = {
+    val url = teamsAndServicesBaseUrl + s"/api/digital-services/${URLEncoder.encode(digitalServiceName, "UTF-8")}"
+
+    http.GET[HttpResponse](url)
+      .map { response =>
+        response.status match {
+          case 200 => Right(timestamp[DigitalService](response))
+          case statusCode => Left(HTTPError(statusCode))
+        }
+      }.recover {
+      case ex =>
+        Logger.error(s"An error occurred getting digitalServiceInfo when connecting to $url: ${ex.getMessage}", ex)
+        Left(ConnectionError(ex))
+    }
+  }
+
+  def allRepositories(implicit hc: HeaderCarrier): Future[Timestamped[Seq[RepositoryDisplayDetails]]] =
+    http.GET[HttpResponse](teamsAndServicesBaseUrl + s"/api/repositories").map {
+      timestamp[Seq[RepositoryDisplayDetails]]
+    }
+
+  def repositoryDetails(name: String)(implicit hc: HeaderCarrier): Future[Option[Timestamped[RepositoryDetails]]] = {
+    http.GET[HttpResponse](teamsAndServicesBaseUrl + s"/api/repositories/$name").map { response =>
+      response.status match {
+        case 404 => None
+        case 200 => Some(timestamp[RepositoryDetails](response))
+      }
+    }
+  }
+
+  def teamsByService(serviceNames: Seq[String])(implicit hc: HeaderCarrier): Future[Timestamped[Map[ServiceName, Seq[TeamName]]]] = {
+    http.POST[Seq[String], HttpResponse](teamsAndServicesBaseUrl + s"/api/services?teamDetails=true", serviceNames).map {
+      timestamp[Map[ServiceName, Seq[TeamName]]]
+    }
+  }
+
+  def allTeamsByService()(implicit hc: HeaderCarrier): Future[Timestamped[Map[ServiceName, Seq[TeamName]]]] = {
+    http.GET[HttpResponse](teamsAndServicesBaseUrl + s"/api/services?teamDetails=true").map {
+      timestamp[Map[ServiceName, Seq[TeamName]]]
+    }
+  }
+
+  def timestamp[T](r: HttpResponse)(implicit fjs: play.api.libs.json.Reads[T]): Timestamped[T] =
+    Timestamped.fromStringInstant(
+      r.json.as[T],
+      r.header(CacheTimestampHeaderName))
+
+  def toCachedItemOption[T](r: HttpResponse)(implicit fjs: play.api.libs.json.Reads[T]): Option[Timestamped[T]] =
+    r.status match {
+      case 404 => None
+      case 200 => Some(timestamp(r))
+    }
+}
+
+object TeamsAndRepositoriesConnector extends TeamsAndRepositoriesConnector {
+  override val http = WSHttp
+
+  override def teamsAndServicesBaseUrl: String = baseUrl("teams-and-services")
+
+  sealed trait TeamsAndRepositoriesError
+
+  case class HTTPError(code: Int) extends TeamsAndRepositoriesError
+
+  case class ConnectionError(exception: Throwable) extends TeamsAndRepositoriesError {
+    override def toString: String = exception.getMessage
+  }
+}
---
+++ app/uk/gov/hmrc/cataloguefrontend/connector/UserManagementConnector.scala
@@ -0,0 +1,259 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.cataloguefrontend
+
+/*
+ * Copyright 2016 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.io.{File, InputStream}
+
+import cats.Applicative
+import cats.data.EitherT
+import play.api.Logger
+import play.api.libs.json._
+import uk.gov.hmrc.cataloguefrontend.FutureHelpers.withTimerAndCounter
+import uk.gov.hmrc.cataloguefrontend.config.WSHttp
+import uk.gov.hmrc.play.config.ServicesConfig
+import uk.gov.hmrc.play.http._
+import uk.gov.hmrc.play.http.logging.MdcLoggingExecutionContext.fromLoggingDetails
+
+import scala.concurrent.{Await, Future}
+import cats.instances.future._
+import play.api.data.validation.ValidationError
+import uk.gov.hmrc.cataloguefrontend.UserManagementConnector.{ConnectionError, getAllUsersFromUMP}
+
+import scala.io.Source
+import scala.concurrent.ExecutionContext.Implicits.global
+
+
+trait UserManagementConnector extends UserManagementPortalLink {
+  val http: HttpGet
+
+  import UserManagementConnector._
+
+  implicit val teamMemberFormat = Json.format[TeamMember]
+  implicit val teamDetailsReads = Json.reads[TeamDetails]
+
+  implicit val httpReads: HttpReads[HttpResponse] = new HttpReads[HttpResponse] {
+    override def read(method: String, url: String, response: HttpResponse) = response
+  }
+
+
+  def getTeamMembersForTeams(teamNames: Seq[String])(implicit hc: HeaderCarrier): Future[Map[String, Either[UMPError, Seq[TeamMember]]]] = {
+
+    def getTeamMembers(teamName: String) = getTeamMembersFromUMP(teamName).map(umpErrorOrTeamMembers => (teamName, umpErrorOrTeamMembers))
+
+    Future.sequence(teamNames.map(getTeamMembers)).map(_.toMap)
+  }
+
+
+  def getTeamMembersFromUMP(team: String)(implicit hc: HeaderCarrier): Future[Either[UMPError, Seq[TeamMember]]] = {
+    val newHeaderCarrier = hc.withExtraHeaders("requester" -> "None", "Token" -> "None")
+
+//    val url = s"$userManagementBaseUrl/v1/organisations/mdtp/teams/$team/members"
+    val url = s"$userManagementBaseUrl/v2/organisations/teams/$team/members"
+
+    def isHttpCodeFailure: Option[(Either[UMPError, _]) => Boolean] =
+      Some { errorOrMembers: Either[UMPError, _] =>
+      errorOrMembers match {
+        case Left(HTTPError(code)) if code != 200 => true
+        case _ => false
+      }
+    }
+
+    val eventualConnectorErrorOrTeamMembers = http.GET[HttpResponse](url)(httpReads, newHeaderCarrier).map { response =>
+      response.status match {
+        case 200 => extractMembers(team, response)
+        case httpCode => Left(HTTPError(httpCode))
+      }
+    }
+
+    withTimerAndCounter("ump") (eventualConnectorErrorOrTeamMembers, isHttpCodeFailure)
+      .recover {
+      case ex =>
+        Logger.error(s"An error occurred when connecting to $userManagementBaseUrl: ${ex.getMessage}", ex)
+        Left(ConnectionError(ex))
+    }
+  }
+
+
+//  def getAllUsersFromUMP_fromfile(): Future[Either[UMPError, Seq[TeamMember]]] = {
+//
+//    val stream: InputStream = getClass.getResourceAsStream("/ump/all-users.json")
+//
+//    val fileContents = Source.fromInputStream(stream).getLines().mkString("\n")
+////    println(fileContents)
+//
+//    val teamMembers = (Json.parse(fileContents) \ "users").validate[Seq[TeamMember]]
+//
+//    teamMembers match {
+//      case JsSuccess(tms, _) =>
+//        println(tms.size)
+//        Future.successful(Right(tms))
+//      case JsError(errors) => Future.successful(Left(ConnectionError(new RuntimeException(errors.toString()))))
+//    }
+//  }
+
+
+  def getAllUsersFromUMP(): Future[Either[UMPError, Seq[TeamMember]]] = {
+
+    val newHeaderCarrier = HeaderCarrier().withExtraHeaders("requester" -> "None", "Token" -> "None")
+
+    val url = s"$userManagementBaseUrl/v2/organisations/users"
+
+    def isHttpCodeFailure: Option[(Either[UMPError, _]) => Boolean] =
+      Some { errorOrMembers: Either[UMPError, _] =>
+        errorOrMembers match {
+          case Left(HTTPError(code)) if code != 200 => true
+          case _ => false
+        }
+      }
+
+    val eventualErrorOrTeamMembers = http.GET[HttpResponse](url)(httpReads, newHeaderCarrier).map { response =>
+      response.status match {
+        case 200 => Right(extractUsers(response))
+        case httpCode => Left(HTTPError(httpCode))
+      }
+    }
+
+    withTimerAndCounter("ump") (eventualErrorOrTeamMembers, isHttpCodeFailure)
+      .recover {
+        case ex =>
+          Logger.error(s"An error occurred when connecting to $url: ${ex.getMessage}", ex)
+          Left(ConnectionError(ex))
+      }
+  }
+
+
+
+
+  def getTeamDetails(team: String)(implicit hc: HeaderCarrier): Future[Either[UMPError, TeamDetails]] = {
+    val newHeaderCarrier = hc.withExtraHeaders("requester" -> "None", "Token" -> "None")
+//    val url = s"$userManagementBaseUrl/v1/organisations/mdtp/teams/$team"
+    val url = s"$userManagementBaseUrl/v2/organisations/teams/$team"
+    withTimerAndCounter("ump-teamdetails") {
+      http.GET[HttpResponse](url)(httpReads, newHeaderCarrier).map { response =>
+        response.status match {
+          case 200 => extractData[TeamDetails](team, response)
+          case httpCode => Left(HTTPError(httpCode))
+        }
+      }.recover {
+        case ex =>
+          Logger.error(s"An error occurred when connecting to $userManagementBaseUrl: ${ex.getMessage}", ex)
+          Left(ConnectionError(ex))
+      }
+    }
+
+  }
+
+
+  def extractData[T](team: String, response: HttpResponse)(implicit rd: Reads[T]): Either[UMPError, T] = {
+
+   Option(response.json).flatMap{ js => js.asOpt[T] } match {
+     case Some(x) => Right(x)
+     case _ => Left(NoData(umpMyTeamsPageUrl(team)))
+   }
+  }
+
+  private def extractMembers(team: String, response: HttpResponse): Either[UMPError, Seq[TeamMember]] = {
+
+    val left = Left(NoData(umpMyTeamsPageUrl(team)))
+
+    val errorOrTeamMembers = (response.json \\ "members")
+      .headOption
+      .map(js => Right(js.as[Seq[TeamMember]]))
+      .getOrElse(left)
+
+    if (errorOrTeamMembers.isRight && errorOrTeamMembers.right.get.isEmpty) left else errorOrTeamMembers
+  }
+
+  def extractUsers(response: HttpResponse): Seq[TeamMember] =
+    (response.json \\ "users")
+      .headOption
+      .map(js => js.as[Seq[TeamMember]])
+      .getOrElse(throw new RuntimeException(s"Unable to parse or extract UMP users: ${response.json}"))
+
+
+
+}
+
+object UserManagementConnector extends UserManagementConnector {
+  override val http = WSHttp
+
+  sealed trait UMPError
+
+  case class NoData(linkToRectify: String) extends UMPError
+
+  case class HTTPError(code: Int) extends UMPError
+
+  case class ConnectionError(exception: Throwable) extends UMPError {
+    override def toString: String = exception.getMessage
+  }
+
+
+  case class TeamMember(displayName: Option[String],
+                        familyName: Option[String],
+                        givenName: Option[String],
+                        primaryEmail: Option[String],
+                        serviceOwnerFor: Option[Seq[String]],
+                        username: Option[String]) {
+
+    def getUmpLink(umpProfileBaseUrl: String) =
+      this.username.map(x => s"${umpProfileBaseUrl.appendSlash}$x").getOrElse("USERNAME NOT PROVIDED")
+
+    def getDisplayName = this.displayName.getOrElse("DISPLAY NAME NOT PROVIDED")
+  }
+
+  case class TeamDetails(description: Option[String],
+                         location: Option[String],
+                         organisation: Option[String],
+                         documentation: Option[String],
+                         slack: Option[String],
+                         team: String)
+
+
+
+
+}
+
+import scala.concurrent.duration._
+
+//object Runner {
+//  def main(args: Array[String]): Unit = {
+//    val allUsers: Future[Either[UserManagementConnector.UMPError, Seq[UserManagementConnector.TeamMember]]] = UserManagementConnector.getAllUsersFromUMP()
+//    Await.result(allUsers, 4 seconds) match {
+//      case Right(users) => println(users)
+//      case Left(e) => println(e)
+//      case x => println(x)
+//    }
+//
+////    Thread.sleep(10000)
+//  }
+//}
