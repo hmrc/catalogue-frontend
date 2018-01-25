a919f079af161044ea144aa244c63110b4346cc2
Konrad <wudkowski@gmail.com>
2018-01-25 15:29:04 +0000

    PLATOPS-1192: Update licences for 2018

--- app/uk/gov/hmrc/cataloguefrontend/CatalogueController.scala
+++ app/uk/gov/hmrc/cataloguefrontend/CatalogueController.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 HM Revenue & Customs
+ * Copyright 2018 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -455,5 +455,3 @@ object DeploymentsFilter {
       .transform[Option[LocalDateTime]](_.flatMap(stringToLocalDateTimeOpt), _.map(_.format(`yyyy-MM-dd`)))
   }
 }
-
-
--- app/uk/gov/hmrc/cataloguefrontend/events/Event.scala
+++ app/uk/gov/hmrc/cataloguefrontend/events/Event.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 HM Revenue & Customs
+ * Copyright 2018 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -46,9 +46,3 @@ case class ServiceOwnerSaveEventData(service: String, displayName: String)
 object ServiceOwnerSaveEventData {
   implicit val serviceOwnerSaveEventDataFormat = Json.format[ServiceOwnerSaveEventData]
 }
-
-
-
-
-
-
