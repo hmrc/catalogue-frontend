bfdb4268fe6e7543007d933e77433919bc28ac1b
colin-lamed <9568290+colin-lamed@users.noreply.github.com>
2021-11-10 11:21:33 +0000

    BDOG-1519 Formatting

--- app/uk/gov/hmrc/cataloguefrontend/shuttering/ShutterService.scala
+++ app/uk/gov/hmrc/cataloguefrontend/shuttering/ShutterService.scala
@@ -26,51 +26,81 @@ import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton
 class ShutterService @Inject() (
-  shutterConnector: ShutterConnector,
+  shutterConnector      : ShutterConnector,
   shutterGroupsConnector: ShutterGroupsConnector,
-  routeRulesConnector: RouteRulesConnector
-)(implicit val ec: ExecutionContext) {
+  routeRulesConnector   : RouteRulesConnector
+)(implicit
+  ec: ExecutionContext
+) {
 
-  def getShutterState(st: ShutterType, env: Environment, serviceName: String)(implicit hc: HeaderCarrier): Future[Option[ShutterState]] =
+  def getShutterState(
+    st         : ShutterType,
+    env        : Environment,
+    serviceName: String
+  )(implicit
+    hc: HeaderCarrier
+  ): Future[Option[ShutterState]] =
     shutterConnector.shutterState(st, env, serviceName)
 
-  def getShutterStates(st: ShutterType, env: Environment)(implicit hc: HeaderCarrier): Future[Seq[ShutterState]] =
+  def getShutterStates(
+    st : ShutterType,
+    env: Environment
+  )(implicit
+    hc: HeaderCarrier
+  ): Future[Seq[ShutterState]] =
     shutterConnector.shutterStates(st, env)
 
   def updateShutterStatus(
-    umpToken: UmpToken,
+    umpToken   : UmpToken,
     serviceName: String,
-    st: ShutterType,
-    env: Environment,
-    status: ShutterStatus
-  )(implicit hc: HeaderCarrier): Future[Unit] =
+    st         : ShutterType,
+    env        : Environment,
+    status     : ShutterStatus
+  )(implicit
+    hc: HeaderCarrier
+  ): Future[Unit] =
     shutterConnector.updateShutterStatus(umpToken, serviceName, st, env, status)
 
-  def outagePage(env: Environment, serviceName: String)(implicit hc: HeaderCarrier): Future[Option[OutagePage]] =
+  def outagePage(
+    env        : Environment,
+    serviceName: String
+  )(implicit
+    hc: HeaderCarrier
+  ): Future[Option[OutagePage]] =
     shutterConnector.outagePage(env, serviceName)
 
-  def frontendRouteWarnings(env: Environment, serviceName: String)(implicit hc: HeaderCarrier): Future[Seq[FrontendRouteWarning]] =
+  def frontendRouteWarnings(
+    env        : Environment,
+    serviceName: String
+  )(implicit
+    hc: HeaderCarrier
+  ): Future[Seq[FrontendRouteWarning]] =
     shutterConnector.frontendRouteWarnings(env, serviceName)
 
-  def findCurrentStates(st: ShutterType, env: Environment)(implicit hc: HeaderCarrier): Future[Seq[ShutterStateData]] =
+  def findCurrentStates(
+    st : ShutterType,
+    env: Environment
+  )(implicit
+    hc: HeaderCarrier
+  ): Future[Seq[ShutterStateData]] =
     for {
       states <- shutterConnector.shutterStates(st, env)
       events <- shutterConnector.latestShutterEvents(st, env)
-      status = states.map { state =>
-                 ShutterStateData(
-                   serviceName = state.name,
-                   shutterType = state.shutterType,
-                   environment = state.environment,
-                   status = state.status,
-                   lastEvent = events.find(_.serviceName == state.name)
-                 )
-               }
-      sorted = status
-                 .sortWith { (l, r) =>
-                   if (l.status.value == r.status.value)
-                     l.serviceName < r.serviceName
-                   else l.status.value == ShutterStatusValue.Shuttered
-                 }
+      status =  states.map { state =>
+                  ShutterStateData(
+                    serviceName = state.name,
+                    shutterType = state.shutterType,
+                    environment = state.environment,
+                    status      = state.status,
+                    lastEvent   = events.find(_.serviceName == state.name)
+                  )
+                }
+      sorted =  status
+                  .sortWith { (l, r) =>
+                    if (l.status.value == r.status.value)
+                      l.serviceName < r.serviceName
+                    else l.status.value == ShutterStatusValue.Shuttered
+                  }
     } yield sorted
 
   /** Creates an [[OutagePageStatus]] for each service based on the contents of [[OutagePage]] */
@@ -80,36 +110,35 @@ class ShutterService @Inject() (
         case Some(outagePage) if outagePage.warnings.nonEmpty =>
           OutagePageStatus(
             serviceName = serviceName,
-            warning = Some(
-              (
-                outagePage.warnings.map(_.message).mkString("<br/>"),
-                outagePage.warnings.head.name match {
-                  case "UnableToRetrievePage"        => "Default outage page will be displayed."
-                  case "MalformedHTML"               => "Outage page will be sent as is, without updating templates."
-                  case "DuplicateTemplateElementIDs" => "All matching elements will be updated"
-                }
-              )
-            )
+            warning     = Some(
+                            ( outagePage.warnings.map(_.message).mkString("<br/>"),
+                              outagePage.warnings.head.name match {
+                                case "UnableToRetrievePage"        => "Default outage page will be displayed."
+                                case "MalformedHTML"               => "Outage page will be sent as is, without updating templates."
+                                case "DuplicateTemplateElementIDs" => "All matching elements will be updated"
+                              }
+                            )
+                          )
           )
         case Some(outagePage) if outagePage.templatedMessages.isEmpty =>
           OutagePageStatus(
             serviceName = serviceName,
-            warning = Some(("No templatedMessage Element in outage-page", "Outage page will be sent as is."))
+            warning     = Some(("No templatedMessage Element in outage-page", "Outage page will be sent as is."))
           )
         case Some(outagePage) if outagePage.templatedMessages.length > 1 =>
           OutagePageStatus(
             serviceName = serviceName,
-            warning = Some(("Multiple templatedMessage Element in outage-page", "All matching elements will be updated."))
+            warning     = Some(("Multiple templatedMessage Element in outage-page", "All matching elements will be updated."))
           )
         case Some(_) =>
           OutagePageStatus(
             serviceName = serviceName,
-            warning = None
+            warning     = None
           )
         case None =>
           OutagePageStatus(
             serviceName = serviceName,
-            warning = Some(("No templatedMessage Element no outage-page", "Default outage page will be displayed."))
+            warning     = Some(("No templatedMessage Element no outage-page", "Default outage page will be displayed."))
           )
       }
     }
@@ -119,11 +148,11 @@ class ShutterService @Inject() (
 
   def lookupShutterRoute(serviceName: String, env: Environment)(implicit hc: HeaderCarrier): Future[Option[String]] =
     for {
-      baseRoutes <- routeRulesConnector.serviceRoutes(serviceName)
-      optFrontendPath = for {
-                          envRoute      <- baseRoutes.find(_.environment == env.asString).map(_.routes)
-                          frontendRoute <- envRoute.find(_.isRegex == false)
-                        } yield ShutterLinkUtils.mkLink(env, frontendRoute.frontendPath)
+      baseRoutes      <- routeRulesConnector.serviceRoutes(serviceName)
+      optFrontendPath =  for {
+                           envRoute      <- baseRoutes.find(_.environment == env.asString).map(_.routes)
+                           frontendRoute <- envRoute.find(_.isRegex == false)
+                         } yield ShutterLinkUtils.mkLink(env, frontendRoute.frontendPath)
 
     } yield optFrontendPath
 }
@@ -132,6 +161,6 @@ case class ShutterStateData(
   serviceName: String,
   shutterType: ShutterType,
   environment: Environment,
-  status: ShutterStatus,
-  lastEvent: Option[ShutterStateChangeEvent]
+  status     : ShutterStatus,
+  lastEvent  : Option[ShutterStateChangeEvent]
 )
