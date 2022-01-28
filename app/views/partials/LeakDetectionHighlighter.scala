a7dee2d5685f19aba99bbd01e5a1eff734b37e56
Phil Evans <65957825+hmrc-phil@users.noreply.github.com>
2022-01-28 11:47:45 +0000

    APPSEC-68 highlight offending leak text

---
+++ app/views/partials/LeakDetectionHighlighter.scala
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views.partials
+
+import play.twirl.api.{Html, HtmlFormat}
+import uk.gov.hmrc.cataloguefrontend.service.LeakDetectionLeakDetails
+
+object LeakDetectionHighlighter extends (LeakDetectionLeakDetails => Html) {
+
+  def apply(l: LeakDetectionLeakDetails): Html = {
+    val spanOpening = "<span class='highlighted'>"
+    val spanEnd     = "</span>"
+
+    val (highlightedErrors, _, lastCursor) = l.matches.foldLeft(("", l.lineText, 0)) {
+      case ((acc, line, cursor), m) =>
+        val beforeMatch      = HtmlFormat.escape(line.substring(cursor, m.start))
+        val highlightedMatch = spanOpening + HtmlFormat.escape(line.substring(m.start, m.end)) + spanEnd
+
+        (acc + beforeMatch + highlightedMatch, line, m.end)
+    }
+
+    HtmlFormat.raw(highlightedErrors + HtmlFormat.escape(l.lineText.substring(lastCursor)))
+  }
+
+}
+
