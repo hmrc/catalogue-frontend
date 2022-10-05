0b8913ebeeb1cd919ccf6ce496aecb972dd398c2
colin-lamed <9568290+colin-lamed@users.noreply.github.com>
2022-10-05 18:42:53 +0100

    Investigate mdc loss - inline play Filter

--- app/uk/gov/hmrc/cataloguefrontend/filters/CacheControlFilter.scala
+++ app/uk/gov/hmrc/cataloguefrontend/filters/CacheControlFilter.scala
@@ -43,7 +43,7 @@ class CacheControlFilter @Inject()(
   configuration: Configuration,
   override val mat: Materializer
 )(implicit ec: ExecutionContext)
-    extends Filter {
+    extends MdtpFilter {
 
   private val logger = Logger(getClass)
 
@@ -54,8 +54,9 @@ class CacheControlFilter @Inject()(
   }
 
   override def apply(f: (RequestHeader) => Future[Result])(rh: RequestHeader): Future[Result] = {
-    logger.info(s"CacheControlFilter: MDC: ${uk.gov.hmrc.play.http.logging.Mdc.mdcData}: ${rh.method} ${rh.uri}")
+    logger.info(s"CacheControlFilter: MDC In: ${uk.gov.hmrc.play.http.logging.Mdc.mdcData}: ${rh.method} ${rh.uri}")
     f(rh).map { result =>
+      logger.info(s"CacheControlFilter: MDC Out: ${uk.gov.hmrc.play.http.logging.Mdc.mdcData}: ${rh.method} ${rh.uri}")
       def status = result.header.status
 
       def existingHeader = result.header.headers.get(CACHE_CONTROL)
