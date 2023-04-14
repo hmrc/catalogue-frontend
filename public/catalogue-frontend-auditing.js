1333a6c9827020354e2f26f41aa22cd7b6340f43
jordanrowe <37838443+jordanrowe@users.noreply.github.com>
2023-04-14 15:25:59 +0100

    trying to get popover links working

--- public/catalogue-frontend-auditing.js
+++ public/catalogue-frontend-auditing.js
@@ -13,10 +13,24 @@ $(function() {
     //   });
     // };
 
+    // most links
     document.querySelectorAll("a").forEach(function(current) {
-        current.addEventListener("click", function(e) { return sendAudit(this.href); });
+        if(current.href.trim().length > 0) { // not empty
+            current.addEventListener("click", function(e) { return sendAudit(this.href); });
+        }
     });
 
+    // links embedded in a bootstrap popover
+    $("[data-toggle=popover]").on("show.bs.popover", function () {
+        document.querySelectorAll("a.popover-link").forEach(function(current) {
+            if(current.href.trim.length > 0) { // not empty
+                console.log("hello")
+                current.addEventListener("click", function(e) { return sendAudit(this.href); });
+            }
+        });
+    });
+
+    // buttons
     document.querySelectorAll("button, [type=button]").forEach(function(current) {
         current.addEventListener("click", function(e) { return sendAudit(this.id); });
     });
