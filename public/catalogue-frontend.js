3f4bc7fa830ef1f53c99a84ba660800db35293ad
victorarbuesmallada <victor.arbues@digital.hmrc.gov.uk>
2023-04-18 13:07:00 +0100

    add gradient mask to service relationships panel change link text to chevrons

--- public/catalogue-frontend.js
+++ public/catalogue-frontend.js
@@ -19,16 +19,21 @@ $(function() { //document ready event
   // Controls the expand/collapse of the what's new and blog post sections on the front page
   $('.board-expand-link').on('click',function(event) {
     event.preventDefault();
-    var growDiv     = $(this).parent().siblings('.board-grow').first();
+    let self = $(this);
+    var growDiv     = self.parent().siblings('.board-grow').first();
     var wrapper     = growDiv.find(".board-collapse");
     var smallHeight = 120; //Should match css value
 
     if (growDiv.height() > smallHeight) {
       growDiv.css("height",smallHeight + "px"); //Collapse to small height
-      $(this).text('See all');
+      growDiv.addClass("board-gradient"); //Add gradient mask
+      self.attr("title", "See all");
+      self.removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down'); //Set expand icon
     } else {
       growDiv.css("height",wrapper.innerHeight() + "px"); //Expand to height of inner content
-      $(this).text('Collapse');
+      growDiv.removeClass("board-gradient"); //Remove gradient mask
+      self.attr("title", "Collapse");
+      self.removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up'); //Set collapse icon
     }
   });
 });
