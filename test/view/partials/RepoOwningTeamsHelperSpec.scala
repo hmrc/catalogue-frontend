7487fd7aa1b1c0b04e941b88b791a072ebe8a361
peteanning <22272449+peteanning@users.noreply.github.com>
2023-06-22 15:25:15 +0100

    BDOG-2605: Helper to sort teamNames

---
+++ test/view/partials/RepoOwningTeamsHelperSpec.scala
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package view.partials
+
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+import uk.gov.hmrc.cataloguefrontend.connector.{GitRepository, RepoType}
+import views.partials.RepoOwningTeamsHelper
+
+import java.time.Instant
+
+class RepoOwningTeamsHelperSpec extends AnyWordSpec with Matchers {
+
+  "An RepoOwningTeamsHelper" should {
+    "sort teamNames by owningTeams" in {
+      val ghr = GitRepository(name = "a",
+        description = "b",
+        githubUrl = "https://github.com/hmrc/a",
+        createdDate = Instant.now(),
+        lastActiveDate = Instant.now(),
+        isPrivate = false,
+        repoType = RepoType.Service,
+        serviceType = None,
+        digitalServiceName = None,
+        owningTeams = Seq("Zoo"),
+        language = None,
+        isArchived = false,
+        defaultBranch = "main",
+        branchProtection = None,
+        isDeprecated = false,
+        teamNames = Seq("Foo", "Bar", "Baz", "Buz", "Zoo")
+        )
+
+      RepoOwningTeamsHelper.sortTeamNamesByOwningTeam(ghr) shouldBe Seq("Zoo", "Foo", "Bar", "Baz", "Buz")
+    }
+  }
+
+}
