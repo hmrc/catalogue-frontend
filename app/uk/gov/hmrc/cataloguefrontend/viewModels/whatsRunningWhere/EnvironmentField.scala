071369bc11fec2aa47622ca8d86c7f43a21c9d4d
MatthewAlexanderBeck <MatthewAlexanderBeck@users.noreply.github.com>
2023-12-05 13:00:12 +0000

    BDOG-2851: Added tooltip

--- app/uk/gov/hmrc/cataloguefrontend/viewModels/whatsRunningWhere/EnvironmentField.scala
+++ app/uk/gov/hmrc/cataloguefrontend/viewModels/whatsRunningWhere/EnvironmentField.scala
@@ -16,6 +16,7 @@
 
 package uk.gov.hmrc.cataloguefrontend.viewModels.whatsRunningWhere
 
+import uk.gov.hmrc.cataloguefrontend.connector.model.Version
 import uk.gov.hmrc.cataloguefrontend.model.Environment
 import uk.gov.hmrc.cataloguefrontend.whatsrunningwhere.WhatsRunningWhereVersion
 
@@ -25,5 +26,52 @@ trait EnvironmentField {
 
 case class EnvironmentWithoutVersion(env: Environment) extends EnvironmentField
 case class EnvironmentWithVersion(env: Environment, version: WhatsRunningWhereVersion) extends EnvironmentField {
-  val isPatchedVersion: Boolean = version.versionNumber.asVersion.patch > 0
+
+  val toVersion: Version        = version.versionNumber.asVersion
+  val isPatchedVersion: Boolean = toVersion.patch > 0
+  def toolTipContent(latestVersion: Version): String = {
+
+    val latestMajorVersion: Int = latestVersion.major
+    val latestMinorVersion: Int = latestVersion.minor
+    val latestPatchVersion: Int = latestVersion.patch
+
+    val thisMajorVersion: Int = toVersion.major
+    val thisMinorVersion: Int = toVersion.minor
+    val thisPatchVersion: Int = toVersion.patch
+
+    val majorVersionContent = if (thisMajorVersion < latestMajorVersion) {
+      val versionDiff: Int = latestMajorVersion - thisMajorVersion
+      val plural = if (versionDiff > 1) "'s" else ""
+      Some(s"${versionDiff} major version${plural} behind")
+    } else None
+
+    val minorVersionContent = if (thisMinorVersion < latestMinorVersion) {
+      val versionDiff: Int = latestMinorVersion - thisMinorVersion
+      val plural = if (versionDiff > 1) "'s" else ""
+      Some(s"${versionDiff} minor version${plural} behind")
+    } else None
+
+    val patchAheadVersionContent = if (toVersion.patch > 0) {
+      val latestNonPatchedVersion = s"${latestMajorVersion}.${latestMinorVersion}.0"
+      val plural = if (thisPatchVersion > 1) "'s" else ""
+      Some(s"${thisPatchVersion} patch/hotfix version${plural} ahead latest minor version [${latestNonPatchedVersion}]")
+    } else None
+
+    val patchBehindVersionContent = if (latestPatchVersion > 0 && toVersion.patch == 0) {
+      val plural = if (latestPatchVersion > 1) "'s" else ""
+      Some(s"${latestPatchVersion} version${plural} behind latest patch/hotfix version [${latestVersion.toString}]. Consider releasing new minor version.")
+    } else None
+
+    val listOfStringsToConcat = List(majorVersionContent, minorVersionContent, patchAheadVersionContent, patchBehindVersionContent)
+
+    val buildHtml: String =
+      s"""
+        |<div><strong>Latest version:</strong> ${latestVersion.toString}</div>
+        |<div><strong>Current version:</strong> ${toVersion.toString}</div>
+        |<ul>
+        | ${listOfStringsToConcat.flatten.map(x => s"<li>${x}</li>").mkString}
+        |</ul>""".stripMargin
+    buildHtml
+  }
+
 }
---
+++ test/uk/gov/hmrc/cataloguefrontend/viewModels/EnvironmentFieldSpec.scala
@@ -0,0 +1,32 @@
+package uk.gov.hmrc.cataloguefrontend.viewModels
+
+import uk.gov.hmrc.cataloguefrontend.util.UnitSpec
+
+class EnvironmentFieldSpec extends UnitSpec {
+
+  "EnvironmentField" should {
+
+    "create tooltip content for major versions" in {
+
+    }
+
+    "create tooltip content for minor versions" in {
+
+    }
+
+    "create tooltip content for patch versions behind" in {
+
+    }
+
+    "create tooltip content for patch versions ahead" in {
+
+    }
+
+    "create tooltip content for a mix of behind versions" in {
+
+
+    }
+
+  }
+
+}
