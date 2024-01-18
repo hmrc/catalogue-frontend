f4343ac73290afb6bfb26ae402ac22f9fed3b945
jordanrowe <37838443+jordanrowe@users.noreply.github.com>
2024-01-18 15:52:03 +0000

    BDOG-2942 controller refactor

--- app/uk/gov/hmrc/cataloguefrontend/createrepository/CreateRepositoryController.scala
+++ app/uk/gov/hmrc/cataloguefrontend/createrepository/CreateRepositoryController.scala
@@ -20,11 +20,11 @@ import cats.data.EitherT
 import cats.implicits._
 import play.api.Logger
 import play.api.i18n.I18nSupport
-import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents, Result}
 import uk.gov.hmrc.cataloguefrontend.auth.CatalogueAuthBuilders
 import uk.gov.hmrc.cataloguefrontend.connector.{BuildDeployApiConnector, TeamsAndRepositoriesConnector}
 import uk.gov.hmrc.cataloguefrontend.connector.model.TeamName
-import uk.gov.hmrc.cataloguefrontend.createrepository.CreateRepositoryController._
+import uk.gov.hmrc.cataloguefrontend.servicecommissioningstatus.{routes => scRoutes}
 import uk.gov.hmrc.http.HeaderCarrier
 import uk.gov.hmrc.internalauth.client._
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendController
@@ -68,31 +68,24 @@ class CreateRepositoryController @Inject()(
       continueUrl = routes.CreateRepositoryController.createServiceRepositoryLanding(),
       retrieval   = Retrieval.locations(resourceType = Some(ResourceType("catalogue-frontend")), action = Some(IAAction("CREATE_REPOSITORY")))
     ).async { implicit request =>
-      val userTeams = cleanseUserTeams(request.retrieval)
-      val submittedForm = CreateServiceRepoForm.form.bindFromRequest()
-
-      submittedForm.fold(
-        formWithErrors => {
-          Future.successful(BadRequest(createRepositoryPage(formWithErrors, userTeams, CreateServiceRepositoryType.values)))
-        },
-        validForm => {
-          import uk.gov.hmrc.cataloguefrontend.servicecommissioningstatus.routes
-          (for {
-            _   <- EitherT.liftF(auth.authorised(Some(createRepositoryPermission(validForm.teamName))))
-            _   <- EitherT(verifyGithubTeamExists(validForm.teamName))
-            res <- EitherT(buildDeployApiConnector.createServiceRepository(validForm)).leftMap[CreateRepoError](ApiError)
-          } yield res).value map {
-            case Right(id) =>
-              logger.info(s"CreateServiceRepository request for ${validForm.repositoryName} successfully sent. Bnd api request id: $id:")
-              Redirect(routes.ServiceCommissioningStatusController.getCommissioningState(validForm.repositoryName))
-            case Left(ApiError(msg)) =>
-              logger.info(s"CreateServiceRepository request for ${validForm.repositoryName} failed with message: $msg")
-              BadRequest(createRepositoryPage(submittedForm.withGlobalError(s"Repository creation failed! Error: $msg"), userTeams, CreateServiceRepositoryType.values))
-            case Left(ValidationError(msg)) =>
-              BadRequest(createRepositoryPage(submittedForm.withError("teamName", msg), userTeams, CreateServiceRepositoryType.values))
-          }
-        }
-      )
+      (for {
+         userTeams     <- EitherT.pure[Future, Result](cleanseUserTeams(request.retrieval))
+         submittedForm =  CreateServiceRepoForm.form.bindFromRequest()
+         validForm     <- submittedForm.fold[EitherT[Future, Result, CreateServiceRepoForm]](
+                            formWithErrors => EitherT.leftT(BadRequest(createRepositoryPage(formWithErrors, userTeams, CreateServiceRepositoryType.values))),
+                            validForm      => EitherT.pure(validForm)
+                          )
+         _             <- EitherT.liftF(auth.authorised(Some(createRepositoryPermission(validForm.teamName))))
+         _             <- EitherT(verifyGithubTeamExists(validForm.teamName))
+                            .leftMap(error => BadRequest(createRepositoryPage(submittedForm.withError("teamName", error), userTeams, CreateServiceRepositoryType.values)))
+         id            <- EitherT(buildDeployApiConnector.createServiceRepository(validForm))
+                            .leftMap{ error =>
+                              logger.info(s"CreateServiceRepository request for ${validForm.repositoryName} failed with message: $error")
+                              BadRequest(createRepositoryPage(submittedForm.withGlobalError(s"Repository creation failed! Error: $error"), userTeams, CreateServiceRepositoryType.values))
+                            }
+         _             =  logger.info(s"CreateServiceRepository request for ${validForm.repositoryName} successfully sent. Bnd api request id: $id:")
+       } yield Redirect(scRoutes.ServiceCommissioningStatusController.getCommissioningState(validForm.repositoryName))
+      ).merge
     }
 
   def createPrototypeRepositoryLanding(): Action[AnyContent] = {
@@ -109,31 +102,24 @@ class CreateRepositoryController @Inject()(
       continueUrl = routes.CreateRepositoryController.createPrototypeRepositoryLanding(),
       retrieval   = Retrieval.locations(resourceType = Some(ResourceType("catalogue-frontend")), action = Some(IAAction("CREATE_REPOSITORY")))
     ).async { implicit request =>
-      val userTeams = cleanseUserTeams(request.retrieval)
-      val submittedForm = CreatePrototypeRepoForm.form.bindFromRequest()
-
-      submittedForm.fold(
-        formWithErrors => {
-          Future.successful(BadRequest(createPrototypePage(formWithErrors, userTeams)))
-        },
-        validForm => {
-          import uk.gov.hmrc.cataloguefrontend.servicecommissioningstatus.routes
-          (for {
-            _   <- EitherT.liftF(auth.authorised(Some(createRepositoryPermission(validForm.teamName))))
-            _   <- EitherT(verifyGithubTeamExists(validForm.teamName))
-            res <- EitherT(buildDeployApiConnector.createPrototypeRepository(validForm)).leftMap[CreateRepoError](ApiError)
-          } yield res).value map {
-            case Right(id)    =>
-              logger.info(s"CreatePrototypeRepository request for ${validForm.repositoryName} successfully sent. Bnd api request id: $id:")
-              Redirect(routes.ServiceCommissioningStatusController.getCommissioningState(validForm.repositoryName))
-            case Left(ApiError(msg)) =>
-              logger.info(s"CreatePrototypeRepository request for ${validForm.repositoryName} failed with message: $msg")
-              BadRequest(createPrototypePage(submittedForm.withGlobalError(s"Repository creation failed! Error: $msg"), userTeams))
-            case Left(ValidationError(msg)) =>
-              BadRequest(createPrototypePage(submittedForm.withError("teamName", msg), userTeams))
-          }
-        }
-      )
+      (for {
+         userTeams     <- EitherT.pure[Future, Result](cleanseUserTeams(request.retrieval))
+         submittedForm =  CreatePrototypeRepoForm.form.bindFromRequest()
+         validForm     <- submittedForm.fold[EitherT[Future, Result, CreatePrototypeRepoForm]](
+                            formWithErrors => EitherT.leftT(BadRequest(createPrototypePage(formWithErrors, userTeams))),
+                            validForm      => EitherT.pure(validForm)
+                          )
+         _             <- EitherT.liftF(auth.authorised(Some(createRepositoryPermission(validForm.teamName))))
+         _             <- EitherT(verifyGithubTeamExists(validForm.teamName))
+                            .leftMap(error => BadRequest(createPrototypePage(submittedForm.withError("teamName", error), userTeams)))
+         id            <- EitherT(buildDeployApiConnector.createPrototypeRepository(validForm))
+                            .leftMap{ error =>
+                              logger.info(s"CreatePrototypeRepository request for ${validForm.repositoryName} failed with message: $error")
+                              BadRequest(createPrototypePage(submittedForm.withGlobalError(s"Repository creation failed! Error: $error"), userTeams))
+                            }
+         _             =  logger.info(s"CreatePrototypeRepository request for ${validForm.repositoryName} successfully sent. Bnd api request id: $id:")
+       } yield Redirect(scRoutes.ServiceCommissioningStatusController.getCommissioningState(validForm.repositoryName))
+      ).merge
     }
 
   def createTestRepositoryLanding(): Action[AnyContent] =
@@ -150,41 +136,34 @@ class CreateRepositoryController @Inject()(
       continueUrl = routes.CreateRepositoryController.createTestRepositoryLanding(),
       retrieval   = Retrieval.locations(resourceType = Some(ResourceType("catalogue-frontend")), action = Some(IAAction("CREATE_REPOSITORY")))
     ).async { implicit request =>
-      val userTeams = cleanseUserTeams(request.retrieval)
-      val submittedForm = CreateTestRepoForm.form.bindFromRequest()
-
-      submittedForm.fold(
-        formWithErrors => {
-          Future.successful(BadRequest(createTestRepositoryPage(formWithErrors, userTeams, CreateTestRepositoryType.values)))
-        },
-        validForm => {
-          import uk.gov.hmrc.cataloguefrontend.servicecommissioningstatus.routes
-          (for {
-            _   <- EitherT.liftF(auth.authorised(Some(createRepositoryPermission(validForm.teamName))))
-            _   <- EitherT(verifyGithubTeamExists(validForm.teamName))
-            res <- EitherT(buildDeployApiConnector.createTestRepository(validForm)).leftMap[CreateRepoError](ApiError)
-          } yield res).value map {
-            case Right(id)    =>
-              logger.info(s"CreateTestRepository request for ${validForm.repositoryName} successfully sent. Bnd api request id: $id:")
-              Redirect(routes.ServiceCommissioningStatusController.getCommissioningState(validForm.repositoryName))
-            case Left(ApiError(msg)) =>
-              logger.info(s"CreateTestRepository request for ${validForm.repositoryName} failed with message: $msg")
-              BadRequest(createTestRepositoryPage(submittedForm.withGlobalError(s"Repository creation failed! Error: $msg"), userTeams, CreateTestRepositoryType.values))
-            case Left(ValidationError(msg)) =>
-              BadRequest(createTestRepositoryPage(submittedForm.withError("teamName", msg), userTeams, CreateTestRepositoryType.values))
-          }
-        }
-      )
+      (for {
+         userTeams     <- EitherT.pure[Future, Result](cleanseUserTeams(request.retrieval))
+         submittedForm =  CreateTestRepoForm.form.bindFromRequest()
+         validForm     <- submittedForm.fold[EitherT[Future, Result, CreateServiceRepoForm]](
+                            formWithErrors => EitherT.leftT(BadRequest(createTestRepositoryPage(formWithErrors, userTeams, CreateTestRepositoryType.values))),
+                            validForm      => EitherT.pure(validForm)
+                          )
+         _             <- EitherT.liftF(auth.authorised(Some(createRepositoryPermission(validForm.teamName))))
+         _             <- EitherT(verifyGithubTeamExists(validForm.teamName))
+                            .leftMap(error => BadRequest(createTestRepositoryPage(submittedForm.withError("teamName", error), userTeams, CreateTestRepositoryType.values)))
+         id            <- EitherT(buildDeployApiConnector.createTestRepository(validForm))
+                            .leftMap{ error =>
+                              logger.info(s"CreateTestRepository request for ${validForm.repositoryName} failed with message: $error")
+                              BadRequest(createTestRepositoryPage(submittedForm.withGlobalError(s"Repository creation failed! Error: $error"), userTeams, CreateTestRepositoryType.values))
+                            }
+         _             =  logger.info(s"CreateTestRepository request for ${validForm.repositoryName} successfully sent. Bnd api request id: $id:")
+       } yield Redirect(scRoutes.ServiceCommissioningStatusController.getCommissioningState(validForm.repositoryName))
+      ).merge
     }
 
   private def verifyGithubTeamExists(
     selectedTeam: TeamName
-  )(implicit hc: HeaderCarrier): Future[Either[CreateRepoError, Unit]] =
+  )(implicit hc: HeaderCarrier): Future[Either[String, Unit]] =
     teamsAndReposConnector.allTeams().map { gitTeams =>
       if(gitTeams.map(_.name).contains(selectedTeam))
         Right(())
       else
-        Left(ValidationError(s"'${selectedTeam.asString}' does not exist as a team on Github."))
+        Left(s"'${selectedTeam.asString}' does not exist as a team on Github.")
     }
 
   private def cleanseUserTeams(resources: Set[Resource]): Seq[TeamName] =
@@ -194,12 +173,4 @@ class CreateRepositoryController @Inject()(
       .map(TeamName.apply)
       .toSeq
       .sorted
-}
-
-object CreateRepositoryController {
-  sealed trait CreateRepoError {
-    val message: String
-  }
-  final case class ValidationError(message: String) extends CreateRepoError
-  final case class ApiError(message: String) extends CreateRepoError
 }
