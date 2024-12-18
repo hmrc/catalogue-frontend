4f62b6c80f96cebc505d68b0f38a7e1a135e0437
Sam Sloman <samsloman@live.co.uk>
2024-12-18 10:00:30 +0000

    BDOG-3303.1: return team admins for team name in quick search

--- app/uk/gov/hmrc/cataloguefrontend/search/SearchIndex.scala
+++ app/uk/gov/hmrc/cataloguefrontend/search/SearchIndex.scala
@@ -108,7 +108,7 @@ class SearchIndex @Inject()(
                           users.filter(u => u.teamNames.contains(t.name) && u.role.asString.contains("admin"))
                                .map(_.username -> t.name.asString)
       teamAdminLinks =  teamAdmins.groupBy(_._1).map: (username, entries) =>
-                          SearchTerm("team admin", username.asString, userRoutes.UsersController.user(username).url, 0.4f, hints = entries.map(_._2).toSet)
+                          SearchTerm("team admin", username.asString, userRoutes.UsersController.user(username).url, 0.5f, hints = entries.map(_._2).toSet)
       allLinks       =  hardcodedLinks ++ teamPageLinks ++ repoLinks ++ serviceLinks ++ commentLinks ++ userLinks ++ teamAdminLinks
     yield cachedIndex.set(optimizeIndex(allLinks))
 
